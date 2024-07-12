2e6e4eaabd01bb1b4c9a9eb42ea93887a87cd9fb
Egan Anderson <149079526+EganAnd@users.noreply.github.com>
2024-07-12 15:00:43 -0700

    add code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const message = document.querySelector('#message').value;
+
+    alert(`Thank you for your message, ${name}! We will get back to you at ${email}.`);
+    
+    document.querySelector('form').reset();
+});
 
